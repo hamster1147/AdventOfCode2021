42068f6c6d4f36067d4e72c34e3e6d346349cd2e
Nicholas Aucoin <hamster1147@gmail.com>
2021-12-01 19:41:35 -0500

    Complete part 2, took an embarrassing amount of time

--- Day1/Day1.java
+++ Day1/Day1.java
@@ -6,28 +6,18 @@ import java.util.Scanner;
 class Day1 {
     public static void main(String[] args) {
         ArrayList<Integer> data;
-        boolean started = false;
-        int prevValue = 0;
-        int total = 0;
 
         if (args.length >= 2) {
-            data = loadData(args[1]);
+            data = parseDataFile(args[1]);
         } else {
-            data = loadData("Day1_Input.csv");
+            data = parseDataFile("Day1_Input.csv");
         }
 
-        for (Integer value : data) {
-            if (started && value.intValue() > prevValue) {
-                total++;
-            }
-            prevValue = value.intValue();
-            started = true;
-        }
-
-        System.out.println("Answer: " + total);
+        System.out.println("Part 1 Answer: " + part1(data));
+        System.out.println("Part 2 Answer: " + part2(data));
     }
 
-    static public ArrayList<Integer> loadData(String filePath) {
+    static public ArrayList<Integer> parseDataFile(String filePath) {
         ArrayList<Integer> data = new ArrayList<Integer>();
 
         try {
@@ -42,5 +32,61 @@ class Day1 {
         }
 
         return data;
+    }
+
+    /**
+     * Part 1 of Day 1 Challenge.
+     * 
+     * @param data List of int values parsed from input file.
+     * @return The answer for part 1.
+     */
+    static public int part1(ArrayList<Integer> data) {
+        boolean started = false;
+        int prevValue = 0;
+        int total = 0;
+
+        for (Integer value : data) {
+            if (started && (value.intValue() > prevValue)) {
+                total++;
+            }
+
+            prevValue = value.intValue();
+            started = true;
+        }
+
+        return total;
+    }
+
+    /**
+     * Part 2 of Day 1 Challenge.
+     * 
+     * @param data List of int values parsed from input file.
+     * @return The answer for part 1.
+     */
+    static public int part2(ArrayList<Integer> data) {
+        int total = 0;
+        ArrayList<Integer> history = new ArrayList<Integer>();
+
+        for (Integer value : data) {
+            history.add(value);
+
+            // Check if the list has too many items
+            if (history.size() > 4) {
+                history.remove(0);
+            }
+
+            if (history.size() == 4) {
+                int commonSum = history.get(1) + history.get(2);
+                // New values are appended, so older values are near the beginning
+                int prevSum = commonSum + history.get(0);
+                int currSum = commonSum + history.get(3);
+
+                if (currSum > prevSum) {
+                    total++;
+                }
+            }
+        }
+
+        return total;
     }
 }
